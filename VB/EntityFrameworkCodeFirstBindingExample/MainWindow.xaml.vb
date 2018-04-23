b4f368279fc9a000edbe42c4f10457bb667a1a42
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/EntityFrameworkCodeFirstBindingExample/MainWindow.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Windows
+
+Namespace EntityFrameworkCodeFirstBindingExample
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits System.Windows.Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+End Namespace
