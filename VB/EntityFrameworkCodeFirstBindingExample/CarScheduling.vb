cd0e5197100928f9456d6e77ee132b34c7a3cd4d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/EntityFrameworkCodeFirstBindingExample/CarScheduling.vb
@@ -0,0 +1,44 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+Imports System
+
+Namespace EntityFrameworkCodeFirstBindingExample
+
+    <Table("CarScheduling")> _
+    Partial Public Class CarScheduling
+        Public Property ID() As Integer
+
+        Public Property CarId() As Integer?
+
+        Public Property UserId() As Integer?
+
+        Public Property Status() As Integer?
+
+        <StringLength(50)> _
+        Public Property Subject() As String
+
+        Public Property Description() As String
+
+        Public Property Label() As Integer?
+
+        Public Property StartTime() As Date?
+
+        Public Property EndTime() As Date?
+
+        <StringLength(50)> _
+        Public Property Location() As String
+
+        Public Property AllDay() As Boolean
+
+        Public Property EventType() As Integer?
+
+        Public Property RecurrenceInfo() As String
+
+        Public Property ReminderInfo() As String
+
+        <Column(TypeName := "smallmoney")> _
+        Public Property Price() As Decimal?
+
+        Public Property ContactInfo() As String
+    End Class
+End Namespace
---
+++ VB/EntityFrameworkCodeFirstBindingExample/ViewModel/MainViewModel.vb
@@ -0,0 +1,43 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Mvvm.POCO
+Imports DevExpress.Xpf.Scheduling
+Imports System.Collections.ObjectModel
+Imports System.Data.Entity
+
+Namespace EntityFrameworkCodeFirstBindingExample
+    <POCOViewModel> _
+    Public Class MainViewModel
+
+        #Region "#instances"
+        Private mySchedulerContext As MySchedulerModel
+        Public Overridable Property Resources() As ObservableCollection(Of Car)
+        Public Overridable Property Appointments() As ObservableCollection(Of CarScheduling)
+        #End Region ' #instances
+
+        #Region "#savechanges"
+        Public Sub AppointmentsUpdated()
+            mySchedulerContext.SaveChanges()
+        End Sub
+        #End Region ' #savechanges
+
+        #Region "#InitNewAppointment"
+        Public Sub InitNewAppointment(ByVal args As AppointmentItemEventArgs)
+            args.Appointment.Reminders.Clear()
+        End Sub
+        #End Region ' #InitNewAppointment
+
+        #Region "#filldata"
+        Protected Sub New()
+            mySchedulerContext = New MySchedulerModel()
+            mySchedulerContext.Cars.Load()
+            Resources = mySchedulerContext.Cars.Local
+            mySchedulerContext.CarSchedulings.Load()
+            Appointments = mySchedulerContext.CarSchedulings.Local
+        End Sub
+        #End Region ' #filldata
+        Public Shared Function Create() As MainViewModel
+            Return ViewModelSource.Create(Function() New MainViewModel())
+        End Function
+    End Class
+End Namespace
