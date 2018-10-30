eb1ba7594fc8721724d27d0e9b2b1efd1b6cfa68
DevExpressExampleBot <fakeEmail>
2018-10-30 19:27:17 +0300

    Source auto update [skip ci]

---
+++ VB/EntityFrameworkCodeFirstBindingExample/Car.vb
@@ -0,0 +1,46 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+
+Namespace EntityFrameworkCodeFirstBindingExample
+
+    Partial Public Class Car
+        Public Property ID() As Integer
+
+        <StringLength(50)> _
+        Public Property Trademark() As String
+
+        <StringLength(50)> _
+        Public Property Model() As String
+
+        Public Property HP() As Short?
+
+        Public Property Liter() As Double?
+
+        Public Property Cyl() As Byte?
+
+        Public Property TransmissSpeedCount() As Byte?
+
+        <StringLength(3)> _
+        Public Property TransmissAutomatic() As String
+
+        Public Property MPG_City() As Byte?
+
+        Public Property MPG_Highway() As Byte?
+
+        <StringLength(7)> _
+        Public Property Category() As String
+
+        Public Property Description() As String
+
+        <StringLength(50)> _
+        Public Property Hyperlink() As String
+
+        <Column(TypeName := "image")> _
+        Public Property Picture() As Byte()
+
+        <Column(TypeName := "money")> _
+        Public Property Price() As Decimal?
+
+        Public Property RtfContent() As String
+    End Class
+End Namespace
---
+++ VB/EntityFrameworkCodeFirstBindingExample/MySchedulerModel.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Data.Entity
+
+Namespace EntityFrameworkCodeFirstBindingExample
+
+    Partial Public Class MySchedulerModel
+        Inherits DbContext
+
+        Public Sub New()
+            MyBase.New("name=MySchedulerModelContext")
+        End Sub
+
+        Public Overridable Property Cars() As DbSet(Of Car)
+        Public Overridable Property CarSchedulings() As DbSet(Of CarScheduling)
+
+        Protected Overrides Sub OnModelCreating(ByVal modelBuilder As DbModelBuilder)
+            modelBuilder.Entity(Of Car)().Property(Function(e) e.Price).HasPrecision(19, 4)
+
+            modelBuilder.Entity(Of CarScheduling)().Property(Function(e) e.Price).HasPrecision(10, 4)
+        End Sub
+    End Class
+End Namespace
