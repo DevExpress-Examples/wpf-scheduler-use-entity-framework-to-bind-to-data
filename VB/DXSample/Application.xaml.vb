2a14858f99db452ec63cdb6fb998f4a5cdbabad9
DevExpressExampleBot <fakeEmail>
2020-02-20 19:48:28 +0300

    autogenerate VB

---
+++ VB/DXSample/Application.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports DevExpress.Internal
+Imports DXSample.Data
+Imports System.Data.Entity
+Imports System.Windows
+
+Namespace DXSample
+	Partial Public Class App
+		Inherits Application
+
+		Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+			DbEngineDetector.PatchConnectionStringsAndConfigureEntityFrameworkDefaultConnectionFactory()
+			Database.SetInitializer(Of SchedulingContext)(New SchedulingContextInitializer())
+			MyBase.OnStartup(e)
+		End Sub
+	End Class
+End Namespace
