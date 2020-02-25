ecc8135c4d9fe0671afdebae58079323171ac8de
DevExpressExampleBot <fakeEmail>
2020-02-25 12:30:16 +0300

    Source auto update [skip ci]

---
+++ VB/DXSample/Views/SchedulingView.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Windows.Controls
+Imports DevExpress.Xpf.Scheduling
+
+Namespace DXSample.Views
+	Partial Public Class SchedulingView
+		Inherits UserControl
+
+		Public ReadOnly Property ChildScheduler() As SchedulerControl
+			Get
+				Return Me.scheduler
+			End Get
+		End Property
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
